445b4c8cba5ea5f0d525114ecbd0189b8237dfa2
LaJordanie <147620767+LaJordanie@users.noreply.github.com>
2023-11-20 16:36:23 +0100

    Main Code

---
+++ BtsSio-Poker/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: etann
+ * Date: 20/11/2023
+ * Heure: 16:26
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+
+namespace BtsSio_Poker
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
